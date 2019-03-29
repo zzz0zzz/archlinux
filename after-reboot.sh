b720534149b3a6785b8f643e3098640433264366
zzz0zzz <40898432+zzz0zzz@users.noreply.github.com>
2019-03-29 18:46:55 +0300

    Update after-reboot.sh

--- after-reboot.sh
+++ after-reboot.sh
@@ -6,7 +6,7 @@ test -f /install-part2.sh && sudo rm /install-part2.sh
 # Some more packages
 sudo pacman -S --noconfirm unzip dash checkbashisms bash-completion noto-fonts \
                            firefox epiphany gnome-keyring emacs gdb git jq rofi \
-                           texlive-most texlive-langextra
+                           texlive-most texlive-langextra alsa-utils
 
 # xorg
 sudo pacman -S --noconfirm xorg xorg-apps
